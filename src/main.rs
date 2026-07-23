7a8936129e33753a27d75dd9154e3a30f8a5d32b
kagamir <64673979+kagamir@users.noreply.github.com>
2026-07-23 10:20:31 +0800

    chore: scaffold dnsbuffer crate with core deps

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
+    tracing::info!("dnsbuffer starting");
+    Ok(())
+}
